755649bdb23f6521ca9fdaa208413be468033ad5
Mustafa Daraghmeh <m_daragh@encs.concordia.ca>
2020-10-18 15:50:28 -0400

    TUT-5

---
+++ src/coen352/tut5/MergeSort.java
@@ -0,0 +1,100 @@
+/*
+ *  2020. Mustafa Daraghmeh.
+ */
+
+package coen352.tut5;
+
+/* Java program for Merge Sort */
+class MergeSort {
+    // Merges two subarrays of arr[].
+    // First subarray is arr[l..m]
+    // Second subarray is arr[m+1..r]
+    void merge(int arr[], int low, int mid, int high) {
+        // Find sizes of two subarrays to be merged
+        int LeftSubArraySize = mid - low + 1;
+        int RightSubArraySize = high - mid;
+
+        /* Create temp arrays */
+        int LeftSubArray[] = new int[LeftSubArraySize];
+        int RightSubArray[] = new int[RightSubArraySize];
+
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < LeftSubArraySize; ++i)
+            LeftSubArray[i] = arr[low + i];
+        for (int j = 0; j < RightSubArraySize; ++j)
+            RightSubArray[j] = arr[mid + 1 + j];
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarry array
+        int k = low;
+        while (i < LeftSubArraySize && j < RightSubArraySize) {
+            if (LeftSubArray[i] <= RightSubArray[j]) {
+                arr[k] = LeftSubArray[i];
+                i++;
+            } else {
+                arr[k] = RightSubArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < LeftSubArraySize) {
+            arr[k] = LeftSubArray[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < RightSubArraySize) {
+            arr[k] = RightSubArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Main function that sorts arr[l..r] using
+    // merge()
+    void sort(int arr[], int l, int r) {
+        if (l < r) {
+            // Find the middle point
+            int m = (l + r) / 2;
+
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+
+    /* A utility function to print array of size n */
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver method
+    public static void main(String args[]) {
+        int arr[] = {12, 11, 13, 5, 6, 7};
+        int arr1[] = {12, 13, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        int arr2[] = {12, 11, 1, 5, 6, 7};
+        System.out.println("Given Array");
+        printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        ob.merge(arr2, 0, 0, 1);
+        printArray(arr2);
+        ob.sort(arr, 0, arr.length - 1);
+
+        System.out.println("\nSorted array");
+        printArray(arr);
+    }
+}
